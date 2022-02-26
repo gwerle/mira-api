3c61c41c88dea73a4db76617060796842c348a55
Gustavo Werle <gustavo.werle2@gmail.com>
2022-02-26 17:11:43 -0300

    feat: add new producers filters and new endpoints

--- src/routes/producer.routes.ts
+++ src/routes/producer.routes.ts
@@ -3,6 +3,8 @@ import CreateProducer from '../services/CreateProducer';
 import GetProducers from '../services/GetProducers';
 import UpdateProducer from '../services/UpdateProducer';
 import DeleteProducer from '../services/DeleteProducer';
+import GetProducerCities from '../services/GetProducerCities';
+import GetProducersEggTypes from '../services/GetProducersEggTypes';
 
 const producerRoutes = Router();
 
@@ -57,21 +59,101 @@ producerRoutes.post('/', async (request, response) => {
 });
 
 producerRoutes.get('/', async (request, response) => {
-  const { productionSystem } = request.query;
+  const { states, city, eggType, productionSystem } = request.query;
 
-  const getProducersService = new GetProducers();
+  const producersByFiltersService = new GetProducers();
+  const filterData: any = {
+    states,
+    eggType,
+    productionSystem,
+    city,
+  };
 
-  const producers = await getProducersService.execute({ productionSystem });
+  const producersByFilters = await producersByFiltersService.execute(
+    filterData,
+  );
 
-  return response.json(producers);
+  return response.json(producersByFilters);
 });
 
-producerRoutes.get('/all', async (request, response) => {
-  const getProducersService = new GetProducers();
+producerRoutes.put('/:id', async (request, response) => {
+  const { id } = request.params;
+  const {
+    farmName,
+    address,
+    district,
+    city,
+    cep,
+    phoneNumber,
+    state,
+    socialMedia,
+    supplyArea,
+    productionSystemEnum,
+    productionSystem,
+    eggType,
+    avgEggProduction,
+    animalsQuantity,
+    permissionToSendInfo,
+    email,
+    moreInformation,
+    lat,
+    long,
+  } = request.body;
 
-  const producers = await getProducersService.getAll();
+  const updateProducer = new UpdateProducer();
 
-  return response.json(producers);
+  const newProducer = await updateProducer.execute({
+    id: Number(id),
+    farmName,
+    address,
+    district,
+    city,
+    cep,
+    phoneNumber,
+    state,
+    socialMedia,
+    supplyArea,
+    productionSystemEnum,
+    productionSystem,
+    eggType,
+    avgEggProduction,
+    animalsQuantity,
+    permissionToSendInfo,
+    email,
+    moreInformation,
+    lat,
+    long,
+  });
+
+  return response.json(newProducer);
+});
+
+producerRoutes.delete('/:id', async (request, response) => {
+  const { id } = request.params;
+
+  const deleteTransaction = new DeleteProducer();
+
+  await deleteTransaction.execute(id);
+
+  return response.status(204).send();
+});
+
+producerRoutes.get('/cities', async (request, response) => {
+  const { state } = request.query;
+
+  const producerCitiesService = new GetProducerCities();
+
+  const producerCities = await producerCitiesService.execute(state as string);
+
+  return response.json(producerCities);
+});
+
+producerRoutes.get('/egg-types', async (request, response) => {
+  const producerEggTypesService = new GetProducersEggTypes();
+
+  const producerEggTypes = await producerEggTypesService.execute();
+
+  return response.json(producerEggTypes);
 });
 
 producerRoutes.put('/:id', async (request, response) => {
--- src/services/GetProducers.ts
+++ src/services/GetProducers.ts
@@ -1,65 +1,61 @@
-import { getRepository } from 'typeorm';
+import { getRepository, In, ILike, FindOperator } from 'typeorm';
 import Producer from '../models/Producer';
 
-interface Request {
-  productionSystem: any;
+export interface RequestI {
+  states?: string[] | string;
+  eggType?: string;
+  productionSystem?: string;
+  city?: string;
+}
+
+interface FilterI {
+  state?: FindOperator<string>;
+  egg_type?: string;
+  production_system?: FindOperator<string>;
+  city?: string;
 }
 
 class GetProducers {
-  public async execute({ productionSystem }: Request): Promise<Producer[]> {
+  public async execute({
+    states,
+    eggType,
+    productionSystem,
+    city,
+  }: RequestI): Promise<Producer[]> {
     const producerRepository = getRepository(Producer);
+    const filterObj: FilterI = {};
 
-    const producers = await producerRepository.query(
-      `SELECT jsonb_build_object(
-          'type',     'FeatureCollection',
-          'name', $1,
-          'features', jsonb_agg(features.feature)
-      )
-      FROM (
-        SELECT jsonb_build_object(
-          'type',       'Feature',
-          'id',         id,
-          'geometry',   ST_AsGeoJSON(geom)::jsonb,
-          'properties', json_build_object(
-              'name', farm_name,
-              'AREA DE FORNECIMENTO', supply_area,
-              'BAIRRO', district,
-              'CIDADE', city,
-              'EMAIL', email,
-              'ENDEREÇO', address,
-              'ESTADO', state,
-              'MÉDIA PROD. OVOS', avg_egg_production,
-              'QTD ANIMAIS', animals_quantity,
-              'REDES SOCIAIS', social_media,
-              'SISTEMA DE PRODUÇÃO', production_system,
-              'TELEFONE', phone_number,
-              'TIPO DE OVO', egg_type
-           )
-        ) AS feature
-        FROM (SELECT * FROM mira_api.public.producers where production_system_enum = $1) inputs) features`,
-      [productionSystem],
-    );
+    if (states && states !== 'null') {
+      let statesFilter;
+      if (Array.isArray(states)) {
+        statesFilter = states;
+      } else {
+        statesFilter = [states];
+      }
 
-    return producers[0].jsonb_build_object;
-  }
+      filterObj.state = In(statesFilter);
+    }
 
-  public async getAll(): Promise<Producer[]> {
-    const producerRepository = getRepository(Producer);
+    if (eggType) {
+      filterObj.egg_type = eggType;
+    }
 
-    const producers = await producerRepository.query(
-      'SELECT id, farm_name, address, district, city, cep, phone_number, state, social_media, supply_area, production_system_enum, production_system, egg_type, avg_egg_production, animals_quantity, permission_to_send_info, email, more_information, ST_X(geom::geometry), ST_Y(geom::geometry) FROM producers',
-      [],
-    );
+    if (productionSystem) {
+      const productionSystemLower = productionSystem.toLowerCase();
+      filterObj.production_system = ILike(`%${productionSystemLower}%`);
+    }
 
-    const producersFormatted = producers.map((item: any) => {
-      return {
-        ...item,
-        lng: item.st_x,
-        lat: item.st_y,
-      };
-    });
+    if (city) {
+      filterObj.city = city;
+    }
 
-    return producersFormatted;
+    const filterResponse = await producerRepository
+      .createQueryBuilder()
+      .select('*, ST_X(geom::geometry) as lng, ST_Y(geom::geometry) as lat')
+      .where(filterObj)
+      .getRawMany();
+
+    return filterResponse;
   }
 }
 
